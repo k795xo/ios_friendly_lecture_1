61ac82383462233177fd825472588f990a0fae26
K Chizhov <k795xo@gmail.com>
2023-07-25 17:13:29 +0300

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Konstantin Chizhov on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
