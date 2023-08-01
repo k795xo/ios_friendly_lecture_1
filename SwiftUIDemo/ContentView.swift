77eaf1005558f283e1b7f23ed56ce3f2c3f4077e
K Chizhov <k795xo@gmail.com>
2023-08-01 12:22:12 +0300

    Решение задания из лекции 1

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -10,10 +10,73 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+                .frame(height: 16)
+            VStack {
+                ZStack {
+                    Circle()
+                        .foregroundColor(.gray)
+                        .frame(width: 128)
+                    Image(systemName: "person.fill")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 64, height: 64)
+                }
+            }
+            Text("+1 (1234) 122-1-22")
+                .font(.largeTitle)
+            HStack {
+                ZStack {
+                    Rectangle()
+                        .cornerRadius(8)
+                        .foregroundColor(.gray)
+                    VStack(spacing: 4) {
+                        Image(systemName: "message.fill")
+                            .foregroundColor(.white)
+                        Text("написать")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+                ZStack {
+                    Rectangle()
+                        .cornerRadius(8)
+                        .foregroundColor(.gray)
+                    VStack(spacing: 4) {
+                        Image(systemName: "phone.fill")
+                            .foregroundColor(.white)
+                        Text("вызов")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+                ZStack {
+                    Rectangle()
+                        .cornerRadius(8)
+                        .foregroundColor(.gray)
+                    VStack(spacing: 4) {
+                        Image(systemName: "video.fill")
+                            .foregroundColor(.white)
+                        Text("видео")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+                ZStack {
+                    Rectangle()
+                        .cornerRadius(8)
+                        .foregroundColor(.gray)
+                    VStack(spacing: 4) {
+                        Image(systemName: "envelope.fill")
+                            .foregroundColor(.white)
+                        Text("почта")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+            .frame(height: 64)
+            Spacer()
         }
         .padding()
     }
